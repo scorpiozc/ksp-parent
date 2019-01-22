ddb4bf32ec89f54e1ee503c23ae126885ae19f61
jdsyly <jdsyly@LAPTOP-O2TUMTT2>
2019-01-22 12:28:29 +0800

    pass common

--- ksp-common/src/main/java/cn/com/bjjdsy/common/constant/CalcPathEnum.java
+++ ksp-common/src/main/java/cn/com/bjjdsy/common/constant/CalcPathEnum.java
@@ -1,15 +1,19 @@
 package cn.com.bjjdsy.common.constant;
 
 public enum CalcPathEnum {
-	RULE(5, 1.5);
+	RULE(5, 1.5, 0.6, 10);
 
-	private CalcPathEnum(int kNum, double transCoeff) {
+	private CalcPathEnum(int kNum, double transCoeff, double m, double u) {
 		this.kNum = kNum;
 		this.transCoeff = transCoeff;
+		this.m = m;
+		this.u = u;
 	}
 
 	private int kNum;
 	private double transCoeff;
+	private double m;
+	private double u;
 
 	public int getkNum() {
 		return kNum;
@@ -25,6 +29,22 @@ public enum CalcPathEnum {
 
 	public void setTransCoeff(double transCoeff) {
 		this.transCoeff = transCoeff;
+	}
+
+	public double getU() {
+		return u;
+	}
+
+	public void setU(double u) {
+		this.u = u;
+	}
+
+	public double getM() {
+		return m;
+	}
+
+	public void setM(double m) {
+		this.m = m;
 	}
 
 }
